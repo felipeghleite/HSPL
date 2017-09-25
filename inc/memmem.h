aa1c46e26662e2de6125428a84f6551729ebb1bd
felipeghleite <felipeghleite@gmail.com>
2017-09-25 14:00:49 -0300

    memmem.c memmem.h added.

---
+++ inc/memmem.h
@@ -0,0 +1,13 @@
+/*
+ * memmem.h
+ *
+ *  Created on: 22 de set de 2017
+ *      Author: Felipe
+ */
+
+#ifndef INC_MEMMEM_H_
+#define INC_MEMMEM_H_
+
+void *memmem(const void *haystack, size_t n, const void *needle, size_t m);
+
+#endif /* INC_MEMMEM_H_ */
